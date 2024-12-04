cf97e4277c7b672f10cd2e7e1d86af49ac86434b
Rajkumar17 <rajkumarm1703@gmail.com>
2024-12-04 19:36:10 +0530

    true or false

---
+++ true_or_false.py
@@ -0,0 +1,8 @@
+# 7 check input is true or false
+n = int(input("enter a number : "))
+if n == 1:
+    print("input is true")
+elif n == 0:
+    print("input is false")
+else:
+    print("enter valid input")
